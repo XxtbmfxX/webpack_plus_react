5a176fc2553084cdcc85b5c5fbbf96296cf6c365
Alexis Toro <aatn1321@gmail.com>
2021-07-08 15:41:19 -0400

    implementation of gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
+import gravatar from "../utils/gravatar";
 
 import '../styles/components/Header.scss';
 
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
-const Header = ({ user }) => {
+const Header = (props) => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
   return (
     <header className="header">
       
@@ -18,20 +21,22 @@ const Header = ({ user }) => {
 
       <div className="header__menu">
         <div className="header__menu--profile">
-          <img src={userIcon} alt="" />
-          <p>Perfil</p>
-          </div>
-            {user.email !== undefined ?
-                <ul>
-                  <li><p>Bienvenido <b>{user.email.slice(0, -9)}</b></p></li>
-                  <li><p>Mi perfil</p></li>
-                  <li><Link to="/register">Registro</Link></li>
-                </ul>
-                :
-                <ul>
-                  <li><Link to="/login">Iniciar sesion</Link></li>
-                </ul>
-            }
+          {hasUser ?
+            <img src={gravatar(user.email)} alt={user.email} /> :
+            <img src={userIcon} alt="" />
+          }
+          {hasUser ?
+            <p>{ user.email.slice(0, -9) }</p> :
+            <p>Perfil</p>
+          }
+        </div>
+        
+          <ul>
+            <li> <Link to="/" >Cuenta</Link> </li>
+            <li> <Link to="/login" >Iniciar sesion</Link> </li>
+            
+          </ul>
+            
       </div>
     </header>
   
@@ -41,7 +46,7 @@ const Header = ({ user }) => {
 
 const mapStateToProps = state => {
   return {
-     user: state.user,
+    user: state.user,
   }
 }
 export default connect(mapStateToProps,null)(Header)
